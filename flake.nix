b4f97ba48344a57a9292b2316ee58e1776742901
Sam Rose <samuel@supabase.io>
2024-12-26 11:28:08 -0500

    julia runner

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Julia data processing application";
+  description = "Julia script runner";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -12,76 +12,27 @@
         pkgs = nixpkgs.legacyPackages.${system};
         
         program = ''
-          #!/usr/bin/env julia
-
-          import Pkg
-          Pkg.add("DataFrames")
-          Pkg.add("CSV")
-
-          using DataFrames, CSV
-
-          abstract type DataSource end
-          abstract type TextSource <: DataSource end
-          abstract type BinarySource <: DataSource end
-
-          struct CSVSource <: TextSource
-              path::String
-          end
-
-          struct JSONSource <: TextSource
-              path::String
-          end
-
-          function read_data(source::CSVSource)
-              CSV.read(source.path, DataFrame)
-          end
-
-          function read_data(source::JSONSource)
-              error("JSON not implemented")
-          end
-
-          abstract type Operation end
-          struct Summarize <: Operation end
-          struct FilterOp <: Operation
-              column::Symbol
-              value::Any
-          end
-
-          function process(df::DataFrame, op::Summarize)
-              describe(df)
-          end
-
-          function process(df::DataFrame, op::FilterOp)
-              filter(row -> row[op.column] == op.value, df)
-          end
-
-          function main()
-              if length(ARGS) < 1
-                  println("Usage: process-data <csv-file>")
-                  exit(1)
-              end
-
-              df = read_data(CSVSource(ARGS[1]))
-              println("Data sample:")
-              println(first(df, 5))
-              
-              println("\nSummary:")
-              println(process(df, Summarize()))
-          end
-
-          main()
+          #!/usr/bin/env bash
+          if [ $# -lt 1 ]; then
+            echo "Usage: run-julia <script.jl> [args...]"
+            exit 1
+          fi
+          
+          SCRIPT="$1"
+          shift
+          exec julia --startup-file=no "$SCRIPT" "$@"
         '';
       in
       {
         packages.default = pkgs.writeShellApplication {
-          name = "process-data";
+          name = "run-julia";
           runtimeInputs = [ pkgs.julia-bin ];
           text = program;
         };
 
         apps.default = {
           type = "app";
-          program = "${self.packages.${system}.default}/bin/process-data";
+          program = "${self.packages.${system}.default}/bin/run-julia";
         };
       });
 }
